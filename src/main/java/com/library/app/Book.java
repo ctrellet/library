7e7e8d6d8af44bd49e67e5ea3fc83db4f516e9d7
tristus <tristus@hotmail.com>
2017-02-14 16:20:18 +0100

    Creation Book et Books non termine

---
+++ src/main/java/com/library/app/Book.java
@@ -0,0 +1,19 @@
+package main.java.com.library.app;
+
+/**
+ * Created by a on 14/02/17.
+ */
+public class Book
+
+
+        private char title;
+        private int pages;
+
+
+        public Book(char title, int pages) {
+            this.title = title;
+            this.pages = pages;
+        }
+
+
+}
--- src/main/java/com/library/app/Books.java
+++ src/main/java/com/library/app/Books.java
@@ -1,7 +1,23 @@
 package main.java.com.library.app;
+import java.util.Arr
 
 /**
  * Created by a on 14/02/17.
  */
 public class Books {
+
+
+    ArrayList<Book> listBooks = new ArrayList<Book>();
+
+    public void putBooks(Book book){
+
+        listBooks.
+
+
+
+    }
+
+
+
+
 }
