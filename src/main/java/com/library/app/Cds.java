b65bb5fea1ad5aa0f8223286fb71e10d32a0b59d
ctrellet <christophe@trellet.ch>
2017-02-15 11:58:59 +0100

    Rename CdRepository => Cds, Update Cds.java LibraryInterface.java

---
+++ src/main/java/com/library/app/Cds.java
@@ -0,0 +1,19 @@
+package com.library.app;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by christophe on 14/02/17.
+ */
+public class Cds {
+
+    private ArrayList<Cd> listCds = new ArrayList<Cd>();
+
+    private ArrayList<Cd> putItems(ArrayList<Cd> newCds ) {
+        listCds.addAll(newCds);
+        return listCds
+    };
+
+
+}
--- src/main/java/com/library/app/LibraryInterface.java
+++ src/main/java/com/library/app/LibraryInterface.java
@@ -4,4 +4,6 @@ package com.library.app;
  * Created by christophe on 14/02/17.
  */
 public interface LibraryInterface {
+
+    ArrayList putItems(ArrayList);
 }
