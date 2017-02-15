fc7d3e40bd30c86b38763dc73d5d6e9762accf49
ctrellet <christophe@trellet.ch>
2017-02-15 17:24:30 +0100

    Test basique, .gitignore, pom

--- src/test/java/com/library/app/LibraryTest.java
+++ src/test/java/com/library/app/LibraryTest.java
@@ -1,38 +1,34 @@
 package com.library.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class LibraryTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public LibraryTest(String testName )
-    {
-        super( testName );
-    }
+public class LibraryTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( LibraryTest.class );
-    }
+     @Test
+     public void baseTest() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        //set
+        Book book1 = new Book("Book1",10);
+        Book book2 = new Book("Book2",20);
+        ArrayList<Book> myBooks = new ArrayList<Book>();
+        myBooks.add(book1);
+        myBooks.add(book2);
+        Library myLib = new Library();
+
+        //test
+        ArrayList<Book> finalList = myLib.putBooks(myBooks);
+        int listSize =  finalList.size();
+
+        //assert
+        assertEquals(2,listSize);
     }
 }
